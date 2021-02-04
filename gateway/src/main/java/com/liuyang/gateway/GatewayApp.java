e16016976717e59c9d56e1566167db2a2c891a09
ly1666 <m13291833189@outlook.com>
2021-02-04 10:11:47 +0800

    部分代码上传

---
+++ gateway/src/main/java/com/liuyang/gateway/GatewayApp.java
@@ -0,0 +1,23 @@
+package com.liuyang.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 
+ * @author 刘阳
+ * @date 2021/2/4 10:10 AM
+ * @since
+ * @version 
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GatewayApp
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
