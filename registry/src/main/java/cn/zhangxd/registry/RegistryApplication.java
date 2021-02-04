bd66c5ba56e6e40e355336e2a7c8a09226000c15
ly1666 <m13291833189@outlook.com>
2021-02-04 10:08:00 +0800

    部分代码上传

--- registry/src/main/java/cn/zhangxd/registry/RegistryApplication.java
+++ registry/src/main/java/cn/zhangxd/registry/RegistryApplication.java
@@ -3,7 +3,18 @@ package cn.zhangxd.registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Description:
+ * @param  
+ * @return 
+ * @throws 
+ * @author 刘阳
+ * @date 2021/2/1 11:35 AM
+ * @since
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryApplication {
